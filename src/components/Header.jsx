2770af95d2392a43238d1240a07be440a3f76f07
SamanthaLavrinc <114955596+SamanthaLavrinc@users.noreply.github.com>
2026-06-30 12:04:09 -0400

    Mobile transition update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,9 @@ export default function Header() {
 
   return (
     <header className="w-full bg-black border-b border-castlepink">
-      <div className="hidden md:flex w-full max-w-[1200px] mx-auto py-4 px-[10px] lg:px-0 flex justify-between items-center gap-4">
+
+      {/* Desktop Header */}
+      <div className="hidden md:flex w-full max-w-[1200px] mx-auto py-4 px-[10px] lg:px-0 justify-between items-center gap-4">
 
         {/* Logo */}
         <div>
@@ -38,6 +40,45 @@ export default function Header() {
         </nav>
 
       </div>
+
+      {/* Mobile Header */}
+      <div className="flex md:hidden w-full px-4 py-4 justify-between items-center">
+
+        {/* Logo */}
+        <img
+          src={logo}
+          alt="Castle Logic Logo"
+          className="max-h-16 w-auto"
+        />
+
+        {/* Hamburger Button */}
+        <button
+          onClick={() => setMenuOpen(!menuOpen)}
+          className="text-castlepink"
+        >
+          <div className="relative w-6 h-6">
+                      <span
+                        className={`absolute left-0 top-2 w-6 h-[2px] rounded-full bg-castlepink origin-center transform transition-all duration-300 ${
+                          menuOpen ? "rotate-45 translate-y-2" : ""
+                        }`}
+                      />
+
+                      <span
+                        className={`absolute left-0 top-4 w-6 h-[2px] rounded-full bg-castlepink origin-center transform transition-all duration-300 ${
+                          menuOpen ? "opacity-0" : ""
+                        }`}
+                      />
+
+                      <span
+                        className={`absolute left-0 top-6 w-6 h-[2px] rounded-full bg-castlepink origin-center transform transition-all duration-300 ${
+                          menuOpen ? "-rotate-45 -translate-y-2" : ""
+                        }`}
+                      />
+                    </div>
+        </button>
+
+      </div>
+
     </header>
   );
 }
