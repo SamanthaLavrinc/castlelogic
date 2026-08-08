f81a91000dadbb6f8cc47e9a51ee907adaf0e021
SamanthaLavrinc <114955596+SamanthaLavrinc@users.noreply.github.com>
2026-08-08 06:59:19 -0400

    Animation update

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -28,7 +28,7 @@ export default function Layout() {
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
-              transition={{ duration: 0.2, ease: "easeInOut" }}
+              transition={{ duration: 0.4, ease: "easeInOut" }}
             >
               <Outlet />
             </motion.div>
