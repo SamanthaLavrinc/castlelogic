888492d823c4a80bdf6f2a2c94d95f64beee552a
SamanthaLavrinc <114955596+SamanthaLavrinc@users.noreply.github.com>
2026-08-09 06:10:34 -0400

    Header update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -59,7 +59,7 @@ export default function Header() {
               to={page.path}
               className="relative group text-castlepurple hover:text-castlepink transition-colors"
             >
-              {page.name === "CONTACT" && <ContactStarburst />}
+              {page.name === "CONTACT" && <ContactStarburst size="w-24 h-24" />}
               {page.name}
 
               <span className="absolute left-0 bottom-[-4px] h-[2px] w-0 bg-castlepink transition-all duration-300 group-hover:w-full"></span>
