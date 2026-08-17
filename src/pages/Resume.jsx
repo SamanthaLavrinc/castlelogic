c5ab914f3913db832cedaf47a44380aca7f1d392
SamanthaLavrinc <114955596+SamanthaLavrinc@users.noreply.github.com>
2026-08-17 05:40:40 -0400

    Update Resume.jsx

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -44,7 +44,10 @@ export default function Resume() {
             <h1 className="text-5xl sm:text-6xl font-bold tracking-wide">SAMANTHA LAVRINC</h1>
             <div className="sm:inline-block">
               <p className="text-castlepurple text-lg sm:text-xl font-light">
-                Founder & Software Engineer – Castle Logic LLC
+                Founder & Software Engineer
+                <br className="sm:hidden" />
+                <span className="hidden sm:inline"> – </span>
+                Castle Logic LLC
               </p>
               <div className="w-16 h-[2px] bg-castlepink mx-auto mt-2 sm:mx-0 sm:mt-2 sm:w-full"></div>
             </div>
