c0e08656804067afcc278e04ba809dafe08c4e9a
SamanthaLavrinc <114955596+SamanthaLavrinc@users.noreply.github.com>
2026-08-22 07:43:04 -0400

    Update Timeline.jsx

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -95,9 +95,9 @@ export default function Timeline() {
                 ) : (
                   <ChevronRight className="text-castlepink transition-all shrink-0" size={24} />
                 )}
-                <h3 className="text-xl font-semibold text-white">{job.title}</h3>
+                <h3 className="text-xl font-semibold text-gray-100">{job.title}</h3>
               </div>
-              <p className="text-lg font-semibold text-white sm:shrink-0">{job.date}</p>
+              <p className="text-lg font-semibold text-grey-100 sm:shrink-0">{job.date}</p>
             </div>
 
             {/* Company */}
